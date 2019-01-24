841c463ccbfb319de77c72a66ba0296475205eac
VINOTH KUMAR <VINOTH KUMAR@Vinoth>
2019-01-24 06:23:06 -0800

    Employee

---
+++ EmployeeDeatils.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class EmployeeDeatils {
+	public static void main(String[] args) {
+		List<Integer> ex = new ArrayList<Integer>();
+		ex.add(10);
+		ex.add(20);
+		ex.add(30);
+		ex.add(40);
+		System.out.println(ex.get(3));
+		System.out.println(ex.get(10));
+}}
